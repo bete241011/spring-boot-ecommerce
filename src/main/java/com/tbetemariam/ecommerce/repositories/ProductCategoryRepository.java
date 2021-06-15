2d30fd5de093eb0bd538d68beebd935d9c077a36
Betemariam <betemariam.tegie@gmail.com>
2021-06-15 18:18:51 +0300

    Added a product category repository.

---
+++ src/main/java/com/tbetemariam/ecommerce/repositories/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.tbetemariam.ecommerce.repositories;
+
+import com.tbetemariam.ecommerce.models.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
